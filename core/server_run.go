6737399f9c3f42c392239c8a333820c1c6d1d882
yaol06 <yaol06@chinatelecom.cn>
2026-05-12 16:35:55 +0800

    feat:add server_run

---
+++ core/server_run.go
@@ -0,0 +1,43 @@
+package core
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+type server interface {
+	ListenAndServe() error
+	Shutdown(context.Context) error
+}
+
+func initServer(address string, router *gin.Engine, readTimeout, writeTimeout time.Duration) {
+	srv := &http.Server{
+		Addr:           address,
+		Handler:        router,
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Printf("listen: %s\n", err)
+			zap.L().Error("server启动失败", zap.Error(err))
+			os.Exit(1)
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+	}
+}
--- go.mod
+++ go.mod
@@ -27,6 +27,8 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	go.uber.org/automaxprocs v1.4.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.39.0 // indirect
 	golang.org/x/net v0.26.0 // indirect
